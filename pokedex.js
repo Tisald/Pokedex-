be0972bd3a8792ca06c7618468ea3f5e267ae4a7
Tisal <pdayarat@uwo.ca>
2022-09-19 14:01:30 -0400

    form verification

--- pokedex.js
+++ pokedex.js
@@ -54,42 +54,10 @@ function searchByName(){
     }
  
 }
+function letters(event) {
+  var key = event.keyCode;
+  return ((key >= 65 && key <= 90) || key == 8);
+};
 
 
 
-// Get the modal
-let modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-let btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-let span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
-
-function open() {
-    modal.style.display = "block";
-  }
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
-
-
-var input3 = document.getElementById("search1");
-input3.addEventListener("keypress", function(event) {
-  if (event.key === "Enter") {
-    
-    document.getElementById("btn1").click();
-    event.preventDefault();
-  }
-});
