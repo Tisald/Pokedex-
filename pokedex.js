2ed6f108124a8c4c8469425b2fca4c02a04ff956
Tisal <pdayarat@uwo.ca>
2022-10-06 15:02:01 -0400

    initial commit

--- pokedex.js
+++ pokedex.js
@@ -7,19 +7,32 @@ function searchByNumber(){
     let b = document.getElementsByClassName("pics");
     console.log(input);
     for(i=0;i<x.length;i++){
-        let temp = x[i].innerHTML;
+        let temp = x[i].firstChild.nodeValue;
 
-    var  names = [];
+    var  names = 0;
         if(!temp.includes(input)){
         console.log("MAtch")
 
-            b[i].style.display = "none";
+          b[i].style.display = "none";
             
+
+
         }
         else{
             b[i].style.display = "block";
         }
     }
+    
+    h4 = document.createElement("h4");
+    h4.textContent = "something";
+    document.body.appendChild(h4);
+
+    // const fragment = document.createDocumentFragment();
+    // const li = fragment
+    //   .appendChild(document.createElement('section'))
+    //   .appendChild(document.createElement('ul'));
+    // ul.textContent = 'hello world';
+    // document.body.appendChild(fragment);
 }
 
 //Search Pokedex by name
@@ -30,7 +43,7 @@ function searchByName(){
     let b = document.getElementsByClassName("pics");
 
     for(i=0;i<x.length;i++){
-        let temp = x[i].innerHTML.toLowerCase();
+        let temp = x[i].firstChild.nodeValue.toLowerCase();
 
         
         if(!temp.includes(input)){
